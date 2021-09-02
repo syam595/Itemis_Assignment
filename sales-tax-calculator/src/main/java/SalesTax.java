ca206c8ace12fdca0295a18d63b6cced1b00612d
Syamkrishna MG <mgsyamkrishna13@gmail.com>
2021-09-02 23:47:51 +0200

    First commit
    Created a basic structure for the sales tax calculator class and corresponding JUnit class

---
+++ sales-tax-calculator/src/main/java/SalesTax.java
@@ -0,0 +1,15 @@
+public class SalesTax {
+    public SalesTax() {
+        // TODO Auto-generated constructor stub
+    }
+
+    int computeSalesTax(String item, double price, boolean importflag)
+    {
+        return 0;
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ sales-tax-calculator/src/test/java/SalesTaxTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class SalesTaxTest {
+    @Test
+    public void testComputeSalesTax() {
+        //fail("Not yet implemented");
+    }
+}
