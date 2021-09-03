b1e312d9d6a26f3286b3d84929ec0fb20c73013b
Syamkrishna MG <mgsyamkrishna13@gmail.com>
2021-09-03 15:48:35 +0200

    Added test cases
    Added the three test cases

--- sales-tax-calculator/src/test/java/SalesTaxTest.java
+++ sales-tax-calculator/src/test/java/SalesTaxTest.java
@@ -1,9 +1,34 @@
 import org.junit.Test;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+
 
 public class SalesTaxTest {
     @Test
-    public void testComputeSalesTax() {
-        fail("Not yet implemented");
+    public void testcase1() {
+        //fail("Not yet implemented");
+        SalesTax salesTax = new SalesTax();
+        assertEquals(salesTax.computeSalesTax("Book",12.49,false),12.49,0.0);
+        assertEquals(salesTax.computeSalesTax("Music CD",14.99,false),16.49,0.0);
+        assertEquals(salesTax.computeSalesTax("Chocolate bar",0.85,false),0.85,0.0);
     }
+
+    @Test
+    public void testcase2() {
+        //fail("Not yet implemented");
+        SalesTax salesTax = new SalesTax();
+        assertEquals(salesTax.computeSalesTax("Box of Chocolate",10.00,true),10.50,0.0);
+        assertEquals(salesTax.computeSalesTax("Bottle of Perfume",47.50,true),54.65,0.0);
+    }
+
+    @Test
+    public void testcase3() {
+        //fail("Not yet implemented");
+        SalesTax salesTax = new SalesTax();
+        assertEquals(salesTax.computeSalesTax("Bottle of Perfume",27.99,true),32.19,0.0);
+        assertEquals(salesTax.computeSalesTax("Bottle of Perfume",18.99,false),20.89,0.0);
+        assertEquals(salesTax.computeSalesTax("Packet of Headache Pills",9.75,true),9.75,0.0);
+        assertEquals(salesTax.computeSalesTax("Box of Chocolate",11.25,true),11.85,0.0);
+
+    }
+
 }
