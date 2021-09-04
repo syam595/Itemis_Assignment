06d49c38144d33b410cebe5dd53905710bc9284b
Syamkrishna MG <mgsyamkrishna13@gmail.com>
2021-09-04 23:34:02 +0200

    Implemented computeSalesTax method

--- sales-tax-calculator/src/main/java/SalesTax.java
+++ sales-tax-calculator/src/main/java/SalesTax.java
@@ -8,80 +8,98 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+
 public class SalesTax {
     public SalesTax() {
         // TODO Auto-generated constructor stub
     }
+
     void readItemDetails() throws IOException {
         List<String> itemInfo = new ArrayList();
         System.out.println("Enter the number of items");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int n = Integer.parseInt(s);
-        double [] priceList = new double[n];
-        String [] quantity = new String[n];
-        String [] item = new String[n];
-        double [] salesTax = new double[n];
+        double[] priceList = new double[n];
+        String[] quantity = new String[n];
+        String[] item = new String[n];
+        double[] salesTax = new double[n];
         System.out.println("Enter the item details");
-        for (int i =0;i<n;i++)
-        {
-            String temp= br.readLine();
+        for (int i = 0; i < n; i++) {
+            String temp = br.readLine();
             itemInfo.add(temp);
         }
-        for (int j=0; j<itemInfo.size(); j++)
-        {
-           String [] temp=   itemInfo.get(j).split(" at ",0);
-           priceList[j]= Double.parseDouble(temp[1]);
-           String [] temp2 = temp[0].split(" ", 2);
-           quantity[j]=temp2[0];
-           item[j]=temp2[1];
-           salesTax[j]=computeSalesTax(item[j],priceList[j]);
-        }
-        for (int k=0;k<priceList.length;k++)
-        {
-            System.out.println(item[k]);
+        for (int j = 0; j < itemInfo.size(); j++) {
+            String[] temp = itemInfo.get(j).split(" at ", 0);
+            priceList[j] = Double.parseDouble(temp[1]);
+            String[] temp2 = temp[0].split(" ", 2);
+            quantity[j] = temp2[0];
+            item[j] = temp2[1];
+            salesTax[j] = computeSalesTax(item[j], priceList[j]);
         }
 
+
     }
-    double computeSalesTax(String item, double price)
-    {
-        return 0;
+
+    double computeSalesTax(String item, double price) {
+        double salesTaxCut = 0.10;
+        double salesTaxImportedCut = 0.05;
+        double salesTax;
+        boolean isImported = isImported(item);
+        if (isImported) {
+            if (checkTaxExemption(item)) {
+                salesTaxCut = salesTaxImportedCut;
+
+            } else {
+                salesTaxCut = salesTaxCut + salesTaxImportedCut;
+
+
+            }
+        } else {
+            if (checkTaxExemption(item)) {
+
+                salesTaxCut = 0.0;
+            }
+        }
+        salesTax = salesTaxCut * price;
+
+        BigDecimal temp = new BigDecimal((roundItemPrice(salesTax)) + price);
+        return temp.setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
-    boolean checkTaxExemption(String item)
-    {
+
+    boolean checkTaxExemption(String item) {
         Set<String> taxExemptedItems = new HashSet<String>();
         taxExemptedItems.add("book");
         taxExemptedItems.add("chocolate");
         taxExemptedItems.add("headache pills");
-        for( String a : taxExemptedItems)
-        {
-            if (item.toLowerCase().contains(a.toLowerCase()))
-            {
+        for (String a : taxExemptedItems) {
+            if (item.toLowerCase().contains(a.toLowerCase())) {
                 return true;
             }
         }
         return false;
 
     }
-    boolean isImported ( String item)
-    {
+
+    boolean isImported(String item) {
         String keyword = "imported";
-        if (item.toLowerCase().contains(keyword.toLowerCase()))
-        {
+        if (item.toLowerCase().contains(keyword.toLowerCase())) {
             return true;
         }
         return false;
     }
-    double roundItemPrice(double price)
-    {
+
+    double roundItemPrice(double price) {
         BigDecimal bPrice = new BigDecimal(price);
         BigDecimal step = new BigDecimal(0.05);
-        return bPrice.divide(step, 0, RoundingMode.UP).multiply(step).setScale(2,RoundingMode.HALF_UP).doubleValue();
-    }
-    void printReciept()
-    {
+        return bPrice.divide(step, 0, RoundingMode.UP).multiply(step).setScale(2, RoundingMode.HALF_UP).doubleValue();
 
     }
+
+    void printReciept() {
+
+    }
+
     public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
         SalesTax salesTax = new SalesTax();
--- sales-tax-calculator/src/test/java/SalesTaxTest.java
+++ sales-tax-calculator/src/test/java/SalesTaxTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
@@ -8,70 +9,66 @@ public class SalesTaxTest {
     public void checkTaxExemptionTest() {
         //fail("Not yet implemented");
         SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.checkTaxExemption("Book"),true);
-        assertEquals(salesTax.checkTaxExemption("headache pills"),true);
-        assertEquals(salesTax.checkTaxExemption("chocolate bar"),true);
-        assertEquals(salesTax.checkTaxExemption("perfume"),false);
+        assertTrue(salesTax.checkTaxExemption("Book"));
+        assertTrue(salesTax.checkTaxExemption("headache pills"));
+        assertTrue(salesTax.checkTaxExemption("chocolate bar"));
+        assertFalse(salesTax.checkTaxExemption("perfume"));
 
     }
+
     @Test
     public void checkTaxExemptionImportedTest() {
         //fail("Not yet implemented");
         SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.checkTaxExemption("Imported chocolate bar"),true);
-        assertEquals(salesTax.checkTaxExemption("Imported Perfume"),false);
+        assertTrue(salesTax.checkTaxExemption("Imported chocolate bar"));
+        assertFalse(salesTax.checkTaxExemption("Imported Perfume"));
 
     }
 
     // Test cases for the method isImported()
 
     @Test
-    public void testcase3() {
-        //fail("Not yet implemented");
+    public void isImportedTest() {
         SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.isImported("Book"),false);
-        assertEquals(salesTax.isImported("headache pills"),false);
-        assertEquals(salesTax.isImported("Imported chocolate bar"),true);
-        assertEquals(salesTax.isImported("imported bottle of perfume"),true);
+        assertFalse(salesTax.isImported("Book"));
+        assertFalse(salesTax.isImported("headache pills"));
+        assertTrue(salesTax.isImported("Imported chocolate bar"));
+        assertTrue(salesTax.isImported("imported bottle of perfume"));
 
     }
 
     // Test cases for the method roundItemprice()
     @Test
     public void roundItempriceTest() {
-        //fail("Not yet implemented");
         SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.roundItemPrice(7.125),7.15,0.0);
-        assertEquals(salesTax.roundItemPrice(0.5625),0.60,0.0);
-        assertEquals(salesTax.roundItemPrice(0.85),0.85,0.0);
+        assertEquals(7.15,salesTax.roundItemPrice(7.125), 0.0);
+        assertEquals(0.60,salesTax.roundItemPrice(0.5625), 0.0);
+        assertEquals(0.85,salesTax.roundItemPrice(0.85),  0.0);
     }
     // Test cases for the method computeSalesTax()
 
     @Test
     public void computeSalesTaxTestSet1() {
-        //fail("Not yet implemented");
         SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.computeSalesTax("book",12.49),12.49,0.0);
-        assertEquals(salesTax.computeSalesTax("music CD",14.99),16.49,0.0);
-        assertEquals(salesTax.computeSalesTax("chocolate bar",0.85),0.85,0.0);
+        assertEquals(12.49,salesTax.computeSalesTax("book", 12.49),  0.0);
+        assertEquals(16.49,salesTax.computeSalesTax("music CD", 14.99),  0.0);
+        assertEquals(0.85,salesTax.computeSalesTax("chocolate bar", 0.85),  0.0);
     }
 
     @Test
     public void computeSalesTaxTestSet2() {
-        //fail("Not yet implemented");
         SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.computeSalesTax("imported box of chocolates",10.00),10.50,0.0);
-        assertEquals(salesTax.computeSalesTax("imported bottle of perfume",47.50),54.65,0.0);
+        assertEquals(10.50,salesTax.computeSalesTax("imported box of chocolates", 10.00),  0.0);
+        assertEquals(54.65,salesTax.computeSalesTax("imported bottle of perfume", 47.50),  0.0);
     }
 
     @Test
     public void computeSalesTaxTestSet3() {
-        //fail("Not yet implemented");
         SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.computeSalesTax("imported bottle of perfume",27.99),32.19,0.0);
-        assertEquals(salesTax.computeSalesTax("bottle of perfume",18.99),20.89,0.0);
-        assertEquals(salesTax.computeSalesTax("packet of headache pills",9.75),9.75,0.0);
-        assertEquals(salesTax.computeSalesTax("box of i mported chocolates",11.25),11.85,0.0);
+        assertEquals(32.19,salesTax.computeSalesTax("imported bottle of perfume", 27.99),  0.0);
+        assertEquals(20.89,salesTax.computeSalesTax("bottle of perfume",18.99),0.0);
+        assertEquals(9.75,salesTax.computeSalesTax("packet of headache pills", 9.75),  0.0);
+        assertEquals(11.85,salesTax.computeSalesTax("box of imported chocolates", 11.25),  0.0);
 
     }
 
